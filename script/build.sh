ac34769ea287565f3fd62d9979b363a7cc5ae305
quokkaman <heesung6701@naver.com>
2022-06-12 15:18:15 +0900

    mkdir out if not exist

--- script/build.sh
+++ script/build.sh
@@ -3,5 +3,6 @@
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 
+mkdir "${BASEDIR}/../out" || :
 cd "${BASEDIR}/../out" || exit
 pyinstaller --onefile -n A_fume_Excel_Converter --paths "${BASEDIR}/../venv/lib/python3.7/site-packages" --clean ../run.py
